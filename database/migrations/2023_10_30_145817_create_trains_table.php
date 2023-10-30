2c56bd3c8f674b68648f7dc136dd00473d15a626
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-30 17:01:44 +0100

    add trains migration file

---
+++ database/migrations/2023_10_30_145817_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departure_point', 50);
+            $table->string('arrival_point', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 10);
+            $table->tinyInteger('wagon_number')->unsigned()->nullable();
+            $table->boolean('in_time')->default(1);
+            $table->boolean('is_canceled')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
